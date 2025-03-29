0e33e954be23439a14561ddc1be7bb4c7e2bdc48
firecat1234 <132525599+firecat1234@users.noreply.github.com>
2025-03-29 17:43:16 -0400

    Delete float_js directory
    merged with frontend

--- float_js/frontend old/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import './style.css'; // Placeholder for styles
-import router from './router'; // Import the router
-
-const app = createApp(App);
-app.use(router);
-app.mount('#app');
